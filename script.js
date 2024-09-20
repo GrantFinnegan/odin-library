4338f0c970345620625c82a173119e34a189c2b7
Grant Finnegan <grantfinnegan@gmail.com>
2024-09-20 12:00:21 -0500

    Add outline of logic to script

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isRead){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(){
+    //retrieve book info from user
+
+    //construct book object
+
+    //display book card on page
+}
